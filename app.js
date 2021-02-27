90f6dcd45f4718c2cf2ee92a768c6dfcbe25433a
SghaierSaifeddine <sghaier.saif.eddine@gmail.com>
2021-02-27 01:17:40 +0100

    test

--- app.js
+++ app.js
@@ -5,42 +5,26 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
 
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 var rateLimit = [];
 
-var db_config = mysql.createConnection({
+var connection = mysql.createPool({
     host: 'us-cdbr-east-03.cleardb.com',
     user: 'b0a27d5fdb9779',
     password: '5e9aed61',
     database: 'heroku_97ca0c133423153'
 });
 
-var connection;
-function handleDisconnect() {
-    connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                    // the old one cannot be reused.
-  
-    connection.connect(function(err) {              // The server is either down
-      if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-      }                                     // to avoid a hot loop, and to allow our node script to
-    });                                     // process asynchronous requests in the meantime.
-                                            // If you're also serving http, display a 503 error.
-    connection.on('error', function(err) {
-      console.log('db error', err);
-      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-        handleDisconnect();                         // lost due to either server restart, or a
-      } else {                                      // connnection idle timeout (the wait_timeout
-        throw err;                                  // server variable configures this)
-      }
-    });
-  }
-  
-  handleDisconnect();
+
+connection.connect(function (err) {
+    if (!err) {
+        console.log("Database is connected ...");
+    } else {
+        console.log(err);
+    }
+});
 
 app.get('/api', (req, res) => {
     res.json({
